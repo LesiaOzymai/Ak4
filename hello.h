2eea692e9e2fb1f06cf86bfc519891ad6dd722f5
Platinumperson <125140988+LesiaOzymai@users.noreply.github.com>
2024-12-08 19:40:41 +0200

    Create hello.h

---
+++ hello.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef HELLO1_H
+#define HELLO1_H
+
+struct event_list {
+    struct list_head list;
+    ktime_t event_time;
+};
+
+void print_hello(void);
+
+#endif // HELLO1_H
